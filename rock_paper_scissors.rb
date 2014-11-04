901c7edc6d51f5c22a3fdb4335055ad39719ef3d
merrillcook0 <cookjm0@sewanee.edu>
2014-11-04 15:55:03 -0600

    Refactored methods to achieve one action each

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,9 +1,12 @@
 #idea: two components. One method gathers new human and computer choices, compares, then renders a verdict. the second method is then called at the end of the first method, to see if the user wants to replay. Only the first method is called.
- 
+
+
+
 puts "Welcome to Rock, Paper, Scissors!"
 
 def say_thanks
   puts "Thanks for playing!"
+  
 end
 
 
@@ -11,34 +14,58 @@ def replay()
   puts "Would you like to play again? (Y or N)"
   response = gets.chomp.upcase
   #ternary--seeing if user says "y" or "n"
-  response == "Y"?  choose_sides : say_thanks
+  response == "Y"?  compare_results(validate_user_input(get_user_input), get_comp_input) : say_thanks
   if (response != "Y") && (response != "N")
+    puts "that is not a valid input"
     replay()
+  elsif (response == "N")
+    return response
+    
   end
+  
 end
 
-def choose_sides 
-  puts "Choose your move: R/P/S..."
-  h =  gets.chomp.upcase
-  computer_choice = ['R', 'P', 'S']
-  c = computer_choice.sample
- 
-    if (h == "R" && c == "S") || (h == "P" && c =="R") || (h =="S" && c == "P")
-      puts "your choice was #{h} and the computer's choice was #{c}, so you won!"
-    elsif (c == "R" && h == "S") || (c == "P" && h == "R")|| (c == "S" && h == "P")
-      puts "your choice was #{h} and the computer's choice was #{c}, so you lost!"
-    else
-      puts "your choice was #{h} and the computer's choice was #{c}....it's a CAT!"
-    end
-#validate request, if not valid, replay still asked
-    if (h != "R") && (h != "P") && (h != "S")
-      puts "Your choice was not valid."
-    end
-#see if the player wants to re-play 
-    replay()
+
+#break into  get_user_input, validate_user_input and compare_result.
+
+def get_user_input ()
+puts "Choose your move: R/P/S..."
+gets.chomp.upcase
 end
+
+def get_comp_input
+    computer_choice = ['R', 'P', 'S']
+  c = computer_choice.sample
+end
+def validate_user_input (user_input)
+  if (user_input != "R") && (user_input != "P") && (user_input != "S")
+    puts "Your Choice was not valid."
+    
+  else
+    user_input 
+  end
+
+end
+
+def compare_results(user_input , computer_input)
+    if (user_input == "R" && computer_input == "S") || (user_input == "P" && computer_input =="R") || (user_input =="S" && computer_input == "P")
+      puts "your choice was #{user_input} and the computer's choice was #{computer_input}, so you won!"
+    elsif (computer_input == "R" && user_input == "S") || (computer_input == "P" && user_input == "R")|| (computer_input == "S" && user_input == "P")
+      puts "your choice was #{user_input} and the computer's choice was #{computer_input}, so you lost!"
+    else
+      puts "your choice was #{user_input} and the computer's choice was #{computer_input}....it's a CAT!"
+    end
+    
+end
+
+
 #call the method
-choose_sides()
+compare_results(validate_user_input(get_user_input), get_comp_input)
+#loops through process until player says they don't want to replay. "Y" replay loops call the above method again.
+begin
+  replay 
+  
+end until replay == "N" 
 
 
 
